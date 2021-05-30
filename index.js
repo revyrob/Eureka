d441690f3bbbd278add4620cb41dbc980560edeb
unknown <kayle.robson@gmail.com>
2021-05-30 02:31:30 +0200

    index.js change in name'

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 
 
 
